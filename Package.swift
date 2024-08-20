f7a3c5f8fbac33bf2297065863c9a030d35e47cd
jenkins <jenkins@regula.by>
2024-08-20 17:49:20 +0300

    Release 7.1.3379

--- Package.swift
+++ Package.swift
@@ -1,21 +1,23 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+let packageName = "DocumentReader"
+
 let package = Package(
     name: "DocumentReader",
     platforms: [.iOS(.v11)],
     products: [
         .library(
             name: "DocumentReader",
-            targets: ["SDKDeps"]),
+            targets: ["\(packageName)Common"]),
     ],
     dependencies: [
-        .package(name: "RegulaCommon", url: "https://github.com/regulaforensics/RegulaCommon-Swift-Package.git", .exact(Version(stringLiteral: "7.3.683"))),
+        .package(name: "RegulaCommon", url: "https://github.com/regulaforensics/RegulaCommon-Swift-Package.git", .exact(Version(stringLiteral: "7.1.379"))),
     ],
     targets: [
-        .binaryTarget(name: "DocumentReader", url: "https://pods.regulaforensics.com/DocumentReader/7.3.3764/DocumentReader-7.3.3764.zip", checksum: "6c93f00e2ead97043a4ade68cb820edebf2a456a76ef7af2d63fa74cd8516ffc"),
+        .binaryTarget(name: "DocumentReader", url: "https://pods.regulaforensics.com/DocumentReader/7.1.3379/DocumentReader-7.1.3379.zip", checksum: "24f3ab840a064180a0e72ad3e22bd260e2595ecd8a4ec20b2e32b01965a9f976"),
         .target(
-            name: "SDKDeps",
+            name: "\(packageName)Common",
             dependencies: [
                 .target(name: "DocumentReader"),
                 .product(name: "RegulaCommon", package: "RegulaCommon")
