e63329b69dc159ad77d19e6b828773be26d0d73a
Jenkins <jenkins@regula.by>
2026-07-14 15:23:29 +0300

    Stage 9.7.6686

--- Package.swift
+++ Package.swift
@@ -1,15 +1,29 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+let packageName = "DocumentReader"
+
 let package = Package(
     name: "DocumentReader",
     platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "DocumentReader",
-            targets: ["DocumentReaderStage"]),
+            targets: ["\(packageName)Common"]),
+    ],
+    dependencies: [
+        .package(name: "RegulaCommon", url: "https://github.com/regulaforensics/RegulaCommon-Swift-Package.git", .exact(Version(stringLiteral: "9.7.2543-stage"))),
     ],
     targets: [
-        .binaryTarget(name: "DocumentReaderStage", url: "https://pods.regulaforensics.com/Stage/DocumentReaderStage/9.7.6684/DocumentReaderStage-9.7.6684.zip", checksum: "0850c689b28fc8eeecfdf7439204d20164f7bad22e9c66c2eeba7ea3f8740ce9"),
+        .binaryTarget(name: "DocumentReader", url: "https://pods.regulaforensics.com/Stage/DocumentReaderStage/9.7.6686/DocumentReaderStage-9.7.6686.zip", checksum: "e3ae529c9e23aef6c4f9c060ab1fb57ea756587d068d3c1664419a3b25b63748"),
+        .target(
+            name: "\(packageName)Common",
+            dependencies: [
+                .target(name: "DocumentReader"),
+                .product(name: "RegulaCommon", package: "RegulaCommon")
+            ],
+            path: "Sources",
+            sources: ["dummy.swift"]
+        )
     ]
 )
