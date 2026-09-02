5cdcb98011c0bac37721140f079eda8691ee1036
Jenkins <jenkins@regula.by>
2026-09-02 04:15:23 +0300

    RC 9.8.6904

--- Package.swift
+++ Package.swift
@@ -12,10 +12,10 @@ let package = Package(
             targets: ["\(packageName)Common"]),
     ],
     dependencies: [
-        .package(name: "RegulaCommon", url: "https://github.com/regulaforensics/RegulaCommon-Swift-Package.git", .exact(Version(stringLiteral: "9.8.2725-nightly"))),
+        .package(name: "RegulaCommon", url: "https://github.com/regulaforensics/RegulaCommon-Swift-Package.git", .exact(Version(stringLiteral: "9.8.2726-rc"))),
     ],
     targets: [
-        .binaryTarget(name: "DocumentReader", url: "https://pods.regulaforensics.com/Nightly/DocumentReaderNightly/9.8.6903/DocumentReaderNightly-9.8.6903.zip", checksum: "0f9940947cfc09d2fb9422c107b76eb3e43943fa21d736966b9404ce871180aa"),
+        .binaryTarget(name: "DocumentReader", url: "https://pods.regulaforensics.com/Stage/DocumentReaderStage/9.8.6904/DocumentReaderStage-9.8.6904.zip", checksum: "2f6b68db675410bc7da8d258dd9bbee9d525dbcc3de1269741b40bde19d23ba5"),
         .target(
             name: "\(packageName)Common",
             dependencies: [
